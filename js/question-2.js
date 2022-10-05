61a751096583bc4bf14a09e2b2e5b0fd0a19eec3
EliseAur <94222653+EliseAur@users.noreply.github.com>
2022-10-05 17:29:25 +0200

    did some changes q.2

--- js/question-2.js
+++ js/question-2.js
@@ -1,6 +1,6 @@
 // Question 2
 
-const apiKey = "bcf5d7365b034e76ba9b9e8fa0f3cb99";
+//const apiKey = "bcf5d7365b034e76ba9b9e8fa0f3cb99";
 
 //const url = `https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=${apiKey}`;
 
@@ -14,16 +14,27 @@ async function getGameFacts() {
         const gameFacts = await response.json();
         console.log(gameFacts);
 
-        resultsContainer.innerHTML = "";
+        const gameList = gameFacts.results;
 
-        for (let i = 0; i < gameFacts.results.length; i++) {
-            console.log(gameFacts.results[i].name);
+        gameContainer.innerHTML = "";
+
+        for (let i = 0; i < gameList.length; i++) {
+            console.log(gameList[i].name);
+            console.log(gameList[i].rating);
+            console.log(gameList[i].tags.length);
 
             if (i === 8) {
                 break;
             }
 
-            gameContainer.innerHTML += `<div class="results">${gameFacts.results[i].name}</div>`;
+            gameContainer.innerHTML += `<div class="results">
+                                            <div>
+                                                <h2>Title: <span class="game-title">${gameList[i].name}</span></h2>
+                                                <ul>
+                                                    <li><span class="bold-text">Rating:</span> ${gameList[i].rating}</li>
+                                                </ul>
+                                            </div>
+                                        </div>`;
         }
     } catch (error) {
         console.log("An error occoured when calling the API:", error);
